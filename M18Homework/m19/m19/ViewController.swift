713b97491374a66e7d3b7f3eea4724a2f08efb05
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-02-21 22:50:58 +0300

    Initial

---
+++ M18Homework/m19/m19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m19
+//
+//  Created by Ka4aH on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
