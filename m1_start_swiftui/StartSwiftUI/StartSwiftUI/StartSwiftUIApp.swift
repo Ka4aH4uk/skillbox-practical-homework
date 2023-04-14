52fb9b8a51164fd97e0d301bf112442dc6ad7889
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-04-14 16:40:41 +0300

    Initial

---
+++ m1_start_swiftui/StartSwiftUI/StartSwiftUI/StartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartSwiftUIApp.swift
+//  StartSwiftUI
+//
+//  Created by Ka4aH on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct StartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
