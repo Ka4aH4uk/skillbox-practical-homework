31f838c7f0b67bd45efd8c9df0adb61e57932999
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-04-14 16:51:10 +0300

    Final version

--- m1_start_swiftui/StartSwiftUI/StartSwiftUI/ContentView.swift
+++ m1_start_swiftui/StartSwiftUI/StartSwiftUI/ContentView.swift
@@ -1,21 +1,29 @@
-//
-//  ContentView.swift
-//  StartSwiftUI
-//
-//  Created by Ka4aH on 14.04.2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var alpha = 0.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Sergey!")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(Color.red)
+                .padding(5)
+            Text("My name is Konstantin!😉")
+                .font(.title2)
+                .foregroundColor(Color.black)
+                .opacity(alpha)
+                .onAppear {
+                    let baseAnimation = Animation.easeInOut(duration: 3)
+                    withAnimation(baseAnimation) {
+                        alpha = 1.0
+                    }
+                }
         }
-        .padding()
     }
 }
 
