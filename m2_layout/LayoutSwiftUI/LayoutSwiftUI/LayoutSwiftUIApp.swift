eff161a9522cc0db2b544ab3adf48fd7d9a406b0
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-04-15 14:36:43 +0300

    Initial

---
+++ m2_layout/LayoutSwiftUI/LayoutSwiftUI/LayoutSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutSwiftUIApp.swift
+//  LayoutSwiftUI
+//
+//  Created by Ka4aH on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct LayoutSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
