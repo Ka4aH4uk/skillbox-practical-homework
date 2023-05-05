87e01ccb04afc000676a5523b7642d30b4676472
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-05-05 22:51:05 +0300

    Task 2

--- m8_combine_p1/M31Combine.playground/Pages/Task 2.xcplaygroundpage/Contents.swift
+++ m8_combine_p1/M31Combine.playground/Pages/Task 2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
-//: [Previous](@previous)
-
 import Foundation
+import Combine
 
-var greeting = "Hello, playground"
+//let url = URL(string: "http://www.iosandroid.ru/")! // networkError
+//let url = URL(string: "http://www.skillbox.ru/")! // 200
+//let url = URL(string: "http://www.skillbox.ru/welcome")! // 404
+let url = URL(string: "http://httpbin.org/status/500")! // invalidResponse
 
-//: [Next](@next)
+enum MyError: Error {
+    case invalidResponse
+    case networkError(error: Error)
+}
+
+let subscription = URLSession.shared.dataTaskPublisher(for: url)
+    .tryMap { data, response -> Int in
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw MyError.invalidResponse
+        }
+        return httpResponse.statusCode
+    }
+    .mapError { error -> MyError in
+        if let error = error as? MyError {
+            return error
+        }
+        return MyError.networkError(error: error)
+    }
+    .sink(receiveCompletion: { completion in
+        switch completion {
+        case .finished:
+            print("Success!")
+        case .failure(error: let error):
+            print("Error: \(error)")
+        }
+    }, receiveValue: { response in
+        print("Response with status code: \(response).")
+    })
+
