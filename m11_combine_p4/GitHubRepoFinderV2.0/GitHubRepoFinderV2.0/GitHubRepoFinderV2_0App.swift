23effcb244df017ea4ac68db672cd073f767fbf2
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-05-22 19:56:28 +0300

    Initial commit

---
+++ m11_combine_p4/GitHubRepoFinderV2.0/GitHubRepoFinderV2.0/GitHubRepoFinderV2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepoFinderV2_0App.swift
+//  GitHubRepoFinderV2.0
+//
+//  Created by Ka4aH on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepoFinderV2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
