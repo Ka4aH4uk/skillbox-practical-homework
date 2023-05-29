cb4cca20f0eb9af0d669e672841ca0cf7b4bc93d
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-05-29 15:04:18 +0300

    Initial commit

---
+++ m12_testing/SkillboxTesting/SkillboxTesting/ContentView.swift
@@ -0,0 +1,61 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var login: String = ""
+    @State var password: String = ""
+    @State var password2: String = ""
+    @State var message: String = ""
+    
+    let loginService = LoginService()
+    
+    var body: some View {
+        TextField("", text: $login, prompt: Text("enter login"))
+                .frame(width: 200)
+                .padding()
+        TextField("", text: $password, prompt: Text("enter password"))
+                .frame(width: 200)
+                .padding()
+        TextField("", text: $password2, prompt: Text("repeat password"))
+                .frame(width: 200)
+                .padding()
+        if message.isEmpty {
+            Text(" ").padding()
+        } else {
+            Text(message).padding()
+        }
+        Button("Register", action: register)
+    }
+    
+    func register() {
+        let result = loginService.checkCredentials(
+            login: login,
+            password: password,
+            password2: password2
+        )
+        
+        switch result {
+        case .invalidLogin:
+            message = "Enter correct email"
+        case .shortPassword:
+            message = "Password should be more than 6 symbols"
+        case .passworNotHaveDigits:
+            message = "Password should have at least one digit"
+        case .passwordNotUppercased:
+            message = "Password should have at least one upper letter"
+        case .passwordNotLowercased:
+            message = "Password should have at least one lower letter"
+        case .passwordHasIllegalCharacters:
+            message = "Password contains illegal characters"
+        case .passwordsMismatch:
+            message = "Passwords are not identical"
+        case .correct:
+            message = "You are successfully registered!"
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
