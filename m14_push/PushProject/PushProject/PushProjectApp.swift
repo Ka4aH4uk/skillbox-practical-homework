daa456668d0d5090ff2bf91f8b31b2593aca1395
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-06-04 12:34:31 +0300

    Initial commit

---
+++ m14_push/PushProject/PushProject/PushProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushProjectApp.swift
+//  PushProject
+//
+//  Created by Ka4aH on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PushProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
